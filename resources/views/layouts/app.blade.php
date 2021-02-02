c83ec44d3d4eaada1ea8c0b770eb9b040deb7047
Gustavo Novaro <gnovaro@gmail.com>
2021-02-02 01:14:20 +0100

    Footer Powered text

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website {{ date('Y') }}</span>
+                        <span>Powered by Roskus</span>
                     </div>
                 </div>
             </footer>
