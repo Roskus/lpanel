7340981996a17f44e3677add3c3d8b1b8cdd7f1d
Gustavo Novaro <gnovaro@gmail.com>
2021-02-04 15:12:04 +0100

    i18n working

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\App;
+/**
+ * Main Controller
+ */
 class MainController extends Controller
 {
     /**
@@ -14,5 +18,11 @@ class MainController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware(function ($request, $next) {
+            if (Auth::check()) {
+                App::setLocale(Auth::user()->lang);
+            }
+            return $next($request);
+        });
     }
 }
