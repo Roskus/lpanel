f158c782a7022431ab64da7bfacd45b6d7914db1
Shift <shift@laravelshift.com>
2025-02-17 09:14:45 +0000

    Adopt anonymous migrations

--- database/migrations/2021_03_13_125332_alter_website_table.php
+++ database/migrations/2021_03_13_125332_alter_website_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterWebsiteTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -42,4 +42,4 @@ class AlterWebsiteTable extends Migration
             $table->dropSoftDeletes();
         });
     }
-}
+};
